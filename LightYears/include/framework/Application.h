e1bda5ff763444bf1dc5d5a0bfdc4bc4b9a00c22
Oleg <olegiseavlp@gmail.com>
2025-11-14 12:05:11 +0200

    Setup the rendering lecture 78

--- LightYears/include/framework/Application.h
+++ LightYears/include/framework/Application.h
@@ -7,9 +7,18 @@ namespace ly
 	{
 	public:
 		Application();
+
 		void Run();
 
 	private:
+		virtual void Tick(float deltaTime);
+		void TickInternal(float deltaTime);
+
+		virtual void Render();
+		void RenderInternal();
+
 		sf::RenderWindow mWindow;
+		float mTargetFrameRate;
+		sf::Clock mTickClock;
 	};
 }
--- LightYears/src/framework/Application.cpp
+++ LightYears/src/framework/Application.cpp
@@ -1,15 +1,21 @@
+#include <iostream>
 #include "framework/Application.h"
 
 namespace ly
 {
 	Application::Application()
-		: mWindow{ sf::VideoMode{720, 900}, "My_Window" }
+		: mWindow{ sf::VideoMode{720, 900}, "My_Window" },
+		mTargetFrameRate{ 60.f },
+		mTickClock{}
 	{
 
 	}
 
 	void Application::Run()
 	{
+		mTickClock.restart();
+		float accumulatedTime = 0.f;
+		float targetDeltaTime = 1.f / mTargetFrameRate;
 		while (mWindow.isOpen())
 		{
 			sf::Event windowEvent;
@@ -20,6 +26,45 @@ namespace ly
 					mWindow.close();
 				}
 			}
+
+			float frameDeltaTime = mTickClock.restart().asSeconds();
+			accumulatedTime += frameDeltaTime;
+			while (accumulatedTime > targetDeltaTime)
+			{
+				accumulatedTime -= targetDeltaTime;
+				TickInternal(targetDeltaTime);
+				RenderInternal();
+			}
+			std::cout << "Ticking at framerate: " << 1.f / frameDeltaTime << "fps." << std::endl;
 		}
+	}
+
+	void Application::TickInternal(float deltaTime)
+	{
+		Tick(deltaTime);
+	}
+
+	void Application::Tick(float deltaTime)
+	{
+		
+	}
+
+	void Application::Render()
+	{
+		mWindow.clear();
+
+		RenderInternal();
+		
+		mWindow.display();
+	}
+
+	void Application::RenderInternal()
+	{
+		sf::CircleShape rect{ 50 };
+		rect.setFillColor(sf::Color::Green);
+		rect.setOrigin(50, 50);
+		rect.setPosition(mWindow.getSize().x / 2.f, mWindow.getSize().y / 2.f);
+
+		mWindow.draw(rect);
 	}
 }
